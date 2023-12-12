5ee712a4b1798252bd25772623a7a9966a73c866
pizza <yulu770520@gmail.com>
2023-12-11 23:38:39 -0500

    yes

---
+++ practice1.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h> 
+using namespace std;
+void solve(){
+}
+
+int main(){
+    int a, b; 
+    cin >> a, b;
+    while(a < b){
+        cout << a << "\n";
+        a++;
+    }
+ }
